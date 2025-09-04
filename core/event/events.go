87dd2650374a454f90b6d7ab0b0c6dfbb2889ee3
touka-aoi <toukaseisei@gmail.com>
2025-09-05 00:02:54 +0900

    add cancel, last, unknown event

--- core/event/events.go
+++ core/event/events.go
@@ -7,17 +7,21 @@ import "fmt"
 type EventType int
 
 const (
-	EVENT_TYPE_ACCEPT EventType = iota
+	EVENT_TYPE_UNKNOWN EventType = iota
+	EVENT_TYPE_ACCEPT
 	EVENT_TYPE_READ
 	EVENT_TYPE_RECVMSG
 	EVENT_TYPE_WRITE
 	EVENT_TYPE_TIMEOUT
+	EVENT_TYPE_CANCEL
 	EVENT_TYPE_SENDMSG
 	EVENT_TYPE_LAST
 )
 
 func (et EventType) String() string {
 	switch et {
+	case EVENT_TYPE_UNKNOWN:
+		return "EVENT_TYPE_UNKNOWN"
 	case EVENT_TYPE_ACCEPT:
 		return "EVENT_TYPE_ACCEPT"
 	case EVENT_TYPE_READ:
@@ -30,6 +34,10 @@ func (et EventType) String() string {
 		return "EVENT_TYPE_RECVMSG"
 	case EVENT_TYPE_SENDMSG:
 		return "EVENT_TYPE_SENDMSG"
+	case EVENT_TYPE_CANCEL:
+		return "EVENT_TYPE_CANCEL"
+	case EVENT_TYPE_LAST:
+		return "EVENT_TYPE_LAST"
 	default:
 		return fmt.Sprintf("UNKNOWN: %d", et)
 	}
